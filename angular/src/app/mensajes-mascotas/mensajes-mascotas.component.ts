b67ef695945020cc95d72ef37c5eee64566712ff
Leandro Gutierrez <leandrogutierrez070@gmail.com>
2018-06-27 14:01:52 -0300

    Posible fix a la logica de MensajesMascotas

--- angular/src/app/mensajes-mascotas/mensajes-mascotas.component.ts
+++ angular/src/app/mensajes-mascotas/mensajes-mascotas.component.ts
@@ -3,25 +3,35 @@ import { Message, MessageService } from "../perfil-mascota/message.service";
 import { Mascota, MascotaService } from "../mascota/mascota.service";
 
 @Component({
-  selector: "app-mensajes-mascotas",
-  templateUrl: "./mensajes-mascotas.component.html",
-  styleUrls: ["./mensajes-mascotas.component.css"]
+    selector: "app-mensajes-mascotas",
+    templateUrl: "./mensajes-mascotas.component.html",
+    styleUrls: ["./mensajes-mascotas.component.css"]
 })
 export class MensajesMascotasComponent implements OnInit {
-  mensajes: Message[] = [];
-  mascotas: Mascota[] = [];
-  constructor(private messageService: MessageService, private mascotaService: MascotaService) { }
+    mensajes: Message[] = [];
+    mascotas: Mascota[] = [];
+    mensajesVista: {
+        mensaje: string,
+        name: string,
+        picture: string,
+    }[] = []
+    constructor(private messageService: MessageService, private mascotaService: MascotaService) { }
 
-  ngOnInit() {
-    this.messageService.getAllMessage().then((data) => {
-      this.mensajes = data;
-        this.mascotaService.buscarMascotas().then((data) => {
-          this.mascotas = data;
-          this.mascotas.forEach(element => {
-            // this.mascotaService.buscarImagen(element.picture).then((data) => ); // guardar images y mascotas juntas en una array
-          });
+    ngOnInit() {
+        this.messageService.getAllMessage().then((data) => {
+            this.mensajes = data;
+            let index = 0
+            this.mensajes.forEach(element => {
+                this.mensajesVista[index] = { mensaje: element.message, picture: '', name: '' }
+                this.mascotaService.buscarMascota(element.pet).then((data) => {
+                    let mascota = data;
+                    this.mensajesVista[index].name = data.name;
+                    this.mascotaService.buscarImagen(mascota.picture).then((data) => {
+                        let image = data;
+                        this.mensajesVista[index].picture = image.image;
+                    });
+                });
+            });
         });
-    });
-  }
-
+    }
 }
